a7381e5656aba67f047bc2e91c195172d5c5619a
MarselDokubo <connectmarsel@gmail.com>
2025-02-03 19:47:50 +0100

    metadata

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -5,7 +5,8 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "QPayy - Price Smarter, Sell Bigger",
-  description: "",
+  description:
+    "Optimize your product pricing across countries to maximize sales. Capture 85% of the untapped market with location-based dynamic pricing",
 };
 
 export default function MarketingLayout({ children }: { children: ReactNode }) {
